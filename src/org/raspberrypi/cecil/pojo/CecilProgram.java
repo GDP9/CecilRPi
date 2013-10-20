f10cfe42ab0fd8243cc08fb38300a04e2f3adefb
cj8g10 <cj8g10@ecs.soton.ac.uk>
2013-10-20 21:37:21 +0100

    Filled out CecilResult and CecilProgram POJOs

--- src/org/raspberrypi/cecil/pojo/CecilProgram.java
+++ src/org/raspberrypi/cecil/pojo/CecilProgram.java
@@ -1,8 +1,40 @@
 package org.raspberrypi.cecil.pojo;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /*
  * Object for encapsulating the programs written by the user
  */
 public class CecilProgram {
 
+	private String programName;
+	private String programAuthor;
+	private Date programDate;
+	private ArrayList<ArrayList<String>> programStatements;
+	
+	public String getProgramName() {
+		return programName;
+	}
+	public void setProgramName(String programName) {
+		this.programName = programName;
+	}
+	public String getProgramAuthor() {
+		return programAuthor;
+	}
+	public void setProgramAuthor(String programAuthor) {
+		this.programAuthor = programAuthor;
+	}
+	public Date getProgramDate() {
+		return programDate;
+	}
+	public void setProgramDate(Date programDate) {
+		this.programDate = programDate;
+	}
+	public ArrayList<ArrayList<String>> getProgramStatements() {
+		return programStatements;
+	}
+	public void setProgramStatements(ArrayList<ArrayList<String>> programStatements) {
+		this.programStatements = programStatements;
+	}
 }
--- src/org/raspberrypi/cecil/pojo/CecilResult.java
+++ src/org/raspberrypi/cecil/pojo/CecilResult.java
@@ -14,4 +14,47 @@ public class CecilResult {
 	private boolean zeroFlag;
 	private boolean negativeFlag;
 	private ArrayList<String> memoryAllocations;
+	
+	public ArrayList<String> getxStack() {
+		return xStack;
+	}
+	public void setxStack(ArrayList<String> xStack) {
+		this.xStack = xStack;
+	}
+	public ArrayList<String> getyStack() {
+		return yStack;
+	}
+	public void setyStack(ArrayList<String> yStack) {
+		this.yStack = yStack;
+	}
+	public ArrayList<String> getAccStack() {
+		return accStack;
+	}
+	public void setAccStack(ArrayList<String> accStack) {
+		this.accStack = accStack;
+	}
+	public boolean isCarryFlag() {
+		return carryFlag;
+	}
+	public void setCarryFlag(boolean carryFlag) {
+		this.carryFlag = carryFlag;
+	}
+	public boolean isZeroFlag() {
+		return zeroFlag;
+	}
+	public void setZeroFlag(boolean zeroFlag) {
+		this.zeroFlag = zeroFlag;
+	}
+	public boolean isNegativeFlag() {
+		return negativeFlag;
+	}
+	public void setNegativeFlag(boolean negativeFlag) {
+		this.negativeFlag = negativeFlag;
+	}
+	public ArrayList<String> getMemoryAllocations() {
+		return memoryAllocations;
+	}
+	public void setMemoryAllocations(ArrayList<String> memoryAllocations) {
+		this.memoryAllocations = memoryAllocations;
+	}
 }
