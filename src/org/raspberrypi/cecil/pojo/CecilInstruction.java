62ddd0cb5073e5d9c7626270b8bd9d29ea562768
Carolz2014 <carolzwebmail@gmail.com>
2013-11-07 15:51:03 +0000

    Comment Formatting

--- src/org/raspberrypi/cecil/pojo/CecilInstruction.java
+++ src/org/raspberrypi/cecil/pojo/CecilInstruction.java
@@ -133,25 +133,25 @@ package org.raspberrypi.cecil.pojo;
 * 		Affected by flags: none
 * 		Affects flags: none
 * 
-* Instruction "ypush"
+* 	Instruction "ypush"
 * 		Machine code: 14
 * 		Full description: Pushes a copy of the number in the Y-register onto the top of the stack.
 * 		Affected by flags: none 
 * 		Affects flags: none
 * 
-* Instruction "ypull"
+* 	Instruction "ypull"
 * 		Machine code: 15
 * 		Full description: pull a copy of the number on  the top of the stack into the Y-register (and remove that number from the stack).
 * 		Affected by flags: none
 * 		Affects flags: none
 * 
-* Instruction "yinc"
+* 	Instruction "yinc"
 * 		Machine code: 16
 *		Full description: This command adds 1 to the current value of the Y-register.
 * 		Affected by flags: none
 * 		Affects flags: sets carry flag if result would have been greater than 1023
 * 
-* Instruction "ydec"
+* 	Instruction "ydec"
 * 		Machine code: 17
 * 		Full description: This command subtracts 1 from the current value of the Y-register. 
 * 						  If this results in a zero or negative result, the appropriate flag is set.
@@ -159,48 +159,48 @@ package org.raspberrypi.cecil.pojo;
 * 		Affects flags: sets zero flag if new Y-register value is zero
 * 					   sets negative flag if new Y-register value is negative
 * 
-* Instruction "load"
+* 	Instruction "load"
 * 		Machine code: 18
 * 		Full description: This instruction replaces the contents of the ACCUMULATOR with the integer stored at the labelled address.
 * 		Affected by flags: none
 * 		Affects flags: none
 * 
 * 
-* Instruction "store"
+* 	Instruction "store"
 * 		Machine code: 19
 * 		Full description: This command will place a copy of the current value in the accumulator at the labelled address (replacing any existing value there).
 * 		Affected by flags: none
 * 		Affects flags: none
 * 
-* Instruction "add"
+* 	Instruction "add"
 * 		Machine code: 20
 * 		Full description: This adds the contents of the number at the data label 'Data', the current value of the accumulator (it also adds to this the current value of the carry flag).
 * 		Affected by flags: will add the contents of the carry flag (1 or 0) to the result
 * 		Affects flags: carry flag set if total of addition is greater than 1023	
 * 					   zero flag set if result of addition is zero
 * 
-* Instruction "sub"
+* 	Instruction "sub"
 * 		Machine code: 21
 * 		Full description: The number at the labelled address is subtracted from the current value of the accumulator, leaving the result in the accumulator.
 * 		Affected by flags: the current value of the carry flag, this is subtracted as well as the number stored at the labelled address
 * 		Affects flags: zero flag set if result is zero 
 * 					   negative flag set if result is negative
 * 
-* Instruction "and"
+* 	Instruction "and"
 * 		Machine code: 22
 * 		Full description: A logical AND is performed between the current value of the ACCUMULATOR and the number at the given address label. The result of the AND is placed in the ACCUMULATOR, and the zero flag set accordingly.	
 *						  I.e. for each of the 10 bits in the ACCUMULATOR and the labelled address an AND is performed, and where both bits are 1, the new bit for the ACCUMULATOR is 1. If either or both bits are 0, then the new bit is 0. 
 * 		Affected by flags: none
 * 		Affects flags: zero flag set if result of AND is zero for all 10 bits
 * 
-* Instruction "or"
+* 	Instruction "or"
 * 		Machine code: 23
 * 		Full description: A logical OR is performed between the current value of the ACCUMULATOR and the number at the given address label. The result of the AND is placed in the ACCUMULATOR, and the zero flag set accordingly.	
 *						  I.e. for each of the 10 bits in the ACCUMULATOR and the labelled address an OR is performed, and where both bits are 1, the new bit for the ACCUMULATOR is 1. If either or both bits are 0, then the new bit is 0. 
 * 		Affected by flags: none
 * 		Affects flags: zero flag set if result of “OR” is zero for all 10 bits
 * 
-* Instruction "xor"
+* 	Instruction "xor"
 * 		Machine code: 24
 * 		Full description: This instruction performs a logical "EXCLUSIVE OR" operation between the bits of the current value of the ACCUMULATOR, and the integer stored at the labelled address.	
 * 						  The result of the “EOR” is stored in the ACCUMULATOR (overwriting the previous value).
@@ -208,63 +208,63 @@ package org.raspberrypi.cecil.pojo;
 * 		Affected by flags: none
 * 		Affects flags: zero flag set if result of “OR” is zero
 * 
-* Instruction "jump"
+* 	Instruction "jump"
 * 		Machine code: 25
 * 		Full description: Pass control to the instructions beginning at the labelled address.	
 *						  (i.e. change the program counter to the labelled address).
 * 		Affected by flags: none
 * 		Affects flags: none (but changes program counter)
 * 
-* Instruction "comp"
+* 	Instruction "comp"
 * 		Machine code: 26
 * 		Full description: Compares the value at the labelled address with the current value of the Accumulator
 * 		Affected by flags: none
 * 		Affects flags: zero flag set if numbers are the same
 * 					   negative flag set if number at labelled address is larger than value in Accumulator
 * 
-* Instruction "jineg"
+* 	Instruction "jineg"
 * 		Machine code: 27
 * 		Full description: If the negative flag is set, THEN jump to a different part of the program (change the program counter to the labelled address), otherwise do nothing.
 * 		Affected by flags: only executed if negative flag is set
 * 		Affects flags: none (but changes program counter)
 * 
-* Instruction "jipos"
+* 	Instruction "jipos"
 * 		Machine code: 28
 * 		Full description: If neither the carry or negative flags are set, THEN jump to a different part of the program (change the program counter to the labelled address), otherwise do nothing.
 * 		Affected by flags: only executed if neither zero flag or negative flag is set
 * 		Affects flags: none (but changes program counter)
 * 
-* Instruction "jizero"
+* 	Instruction "jizero"
 * 		Machine code: 29
 * 		Full description: If the zero flag is set, THEN jump to a different part of the program (change the program counter to the labelled address), otherwise do nothing.
 * 		Affected by flags: only executed if zero flag is set	
 * 		Affects flags: none (but changes program counter)
 * 
-* Instruction "jmptosr"
+* 	Instruction "jmptosr"
 * 		Machine code: 30
 * 		Full description: Pass control to the instructions beginning at the labelled address, however when the next “return” instruction is executed, program control will return to the instruction immediately following the “jmptosr” instruction.
 * 		Affected by flags: none
 * 		Affects flags: none (but changes program counter)
 * 
-* Instruction "jicarry"
+* 	Instruction "jicarry"
 * 		Machine code: 31
 * 		Full description: If the carry flag is set, THEN jump to a different part of the program (change the program counter to the labelled address), otherwise do nothing.
 * 		Affected by flags: only executed if carry flag is set	
 * 		Affects flags: none (but changes program counter)
 * 
-* Instruction "xload"
+* 	Instruction "xload"
 * 		Machine code: 32
 * 		Full description: A copy of the number stored in the labelled address is copied into the X-register.
 * 		Affected by flags: none
 * 		Affects flags: none
 * 
-* Instruction "xstore"
+* 	Instruction "xstore"
 * 		Machine code: 33
 * 		Full description: The current value of the X-register is copied into the labelled address (the X-register is unchanged).
 * 		Affected by flags: none
 * 		Affects flags: none
 * 
-* Instruction "loadmx"
+* 	Instruction "loadmx"
 * 		Machine code: 34
 * 		Full description: This instruction is similar to "load", but includes the current value of the X-register when working out from which memory address to copy the data into the accumulator. 	
 * 						  i.e.
@@ -274,32 +274,32 @@ package org.raspberrypi.cecil.pojo;
 * 		Affected by flags: none
 * 		Affects flags: none
 * 
-* Instruction "xcomp"
+* 	Instruction "xcomp"
 * 		Machine code: 35
 * 		Full description: The value at the labelled address is compared with the current value of the X-register (rather than with the ACCUMULATOR for the “comp” instruction).
 * 		Affected by flags: none
 * 		Affects flags: zero flag set if numbers are the same
 * 					   negative flag set if number at labelled address is larger than value in X-register
 * 
-* Instruction "yload"
+* 	Instruction "yload"
 * 		Machine code: 36
 * 		Full description: A copy of the number stored in the labelled address is copied into the Y-register.
 * 		Affected by flags: none
 * 		Affects flags: none
 * 
-* Instruction "ystore"
+* 	Instruction "ystore"
 * 		Machine code: 37
 * 		Full description: The current value of the Y-register is copied into the labelled address (the Y-register is unchanged).
 * 		Affected by flags: none
 * 		Affects flags: none
 * 
-* Instruction "insert"
+* 	Instruction "insert"
 * 		Machine code: 38
 * 		Full description: Instructs the compiler to store the given integer as a 10-bit word in memory — an address label is optional (but usually present).
 * 		Affected by flags: none
 * 		Affects flags: none
 * 
-* Instruction "ycomp"
+* 	Instruction "ycomp"
 * 		Machine code: 39
 * 		Full description: The value at the labelled address is compared with the current value of the Y-register (rather than with the ACCUMULATOR for the “comp” instruction).
 * 		Affected by flags: none
