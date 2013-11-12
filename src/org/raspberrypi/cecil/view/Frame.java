5c34cd52d6a8caaa2d25401dd6ce0df3bc1e62a5
karishmakiran <karishmakiran23@gmail.com>
2013-11-12 16:50:19 +0000

    Changes to autocomplete comboBox

--- src/org/raspberrypi/cecil/view/Frame.java
+++ src/org/raspberrypi/cecil/view/Frame.java
@@ -74,6 +74,7 @@ import javax.swing.border.TitledBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
+import org.jdesktop.swingx.autocomplete.ComboBoxCellEditor;
 import org.raspberrypi.cecil.controller.Controller;
 import org.raspberrypi.cecil.model.outputstream.OutputError;
 import org.raspberrypi.cecil.pojo.Instruction;
@@ -161,7 +162,7 @@ public class Frame extends JFrame implements ViewInterface {
 	
 	//Misc
 	private JTextField input;
-	private Java2sAutoComboBox comboBox;
+	private JComboBox comboBox;
 	private FontUIResource currentFont;
 	private ArrayList<ArrayList<String>> currentProgram;
 	private Color[] currentTheme;
@@ -562,21 +563,28 @@ public class Frame extends JFrame implements ViewInterface {
 		tblInput.getColumnModel().getColumn(0).setMinWidth(35);
 		tblInput.getColumnModel().getColumn(0).setMaxWidth(35);
 		
-		comboBox = new Java2sAutoComboBox(instructionList);
+		comboBox = new JComboBox(instructionList.toArray());
 		comboBox.setEditable(true);
-		comboBox.setRenderer(new MyComboBoxRenderer());
-//		AutoCompleteDecorator.decorate(comboBox);
-		tblInput.getColumnModel().getColumn(2).setCellEditor(new DefaultCellEditor(comboBox));
-		
+		AutoCompleteDecorator.decorate(comboBox);
+									
+				ActionListener actionListener = new ActionListener() {
+					public void actionPerformed(ActionEvent actionEvent) {
+						actionEvent.getSource();
+					}
+				};
+				comboBox.addActionListener(actionListener);				
+				comboBox.setRenderer(new MyComboBoxRenderer()); 				
 		input = new JTextField();
 		DefaultTableCellRenderer aligncenter = new DefaultTableCellRenderer();
        	aligncenter.setHorizontalAlignment(JLabel.CENTER);
        	tblInput.getColumnModel().getColumn(0).setCellRenderer(aligncenter);
        	tblInput.setRowHeight(25);                
        	tblInput.getColumnModel().getColumn(1).setCellEditor(new DefaultCellEditor(input));
+       	tblInput.getColumnModel().getColumn(2).setCellEditor(new ComboBoxCellEditor(comboBox));       	
        	tblInput.getColumnModel().getColumn(3).setCellEditor(new DefaultCellEditor(input));
        	tblInput.getColumnModel().getColumn(4).setCellEditor(new DefaultCellEditor(input));
        	tblInput.setFillsViewportHeight(true);
+		
 		tblInput.addKeyListener(new KeyListener() {
 			public void keyPressed(KeyEvent e) {
 				int selectedRow = tblInput.getSelectedRow();
@@ -994,210 +1002,7 @@ public class Frame extends JFrame implements ViewInterface {
 		}
 	}
 	
-	//TODO Javadocing needed for everything in here
-	public class Java2sAutoTextField extends JTextField {
-		class AutoDocument extends PlainDocument {
-			public void replace(int i, int j, String s, AttributeSet attributeset) throws BadLocationException {
-				super.remove(i, j);
-				insertString(i, s, attributeset);
-			}
-
-			public void insertString(int i, String s, AttributeSet attributeset) throws BadLocationException {
-				if (s == null || "".equals(s))
-					return;
-				String s1 = getText(0, i);
-				String s2 = getMatch(s1 + s);
-				int j = (i + s.length()) - 1;
-				if (isStrict && s2 == null) {
-					s2 = getMatch(s1);
-					j--;
-				} else if (!isStrict && s2 == null) {
-					super.insertString(i, s, attributeset);
-					return;
-				}
-				if (autoComboBox != null && s2 != null)
-					autoComboBox.setSelectedValue(s2);
-				super.remove(0, getLength());
-				super.insertString(0, s2, attributeset);
-				setSelectionStart(j + 1);
-				setSelectionEnd(getLength());
-			}
-
-			public void remove(int i, int j) throws BadLocationException {
-				int k = getSelectionStart();
-				if (k > 0)
-					k--;
-				String s = getMatch(getText(0, k));
-				if (!isStrict && s == null) {
-					super.remove(i, j);
-				} else {
-					super.remove(0, getLength());
-					super.insertString(0, s, null);
-				}
-				if (autoComboBox != null && s != null)
-					autoComboBox.setSelectedValue(s);
-				try {
-					setSelectionStart(k);
-					setSelectionEnd(getLength());
-				} catch (Exception exception) {
-				}
-			}
-		}
-
-		public Java2sAutoTextField(List list) {
-			isCaseSensitive = false;
-			isStrict = true;
-			autoComboBox = null;
-			if (list == null) {
-				throw new IllegalArgumentException("values can not be null");
-			} else {
-				dataList = list;
-				init();
-				return;
-			}
-		}
-
-		Java2sAutoTextField(List list, Java2sAutoComboBox b) {
-			isCaseSensitive = false;
-			isStrict = true;
-			autoComboBox = null;
-		    if (list == null) {
-		    	throw new IllegalArgumentException("values can not be null");
-		    } else {
-		    	dataList = list;
-		    	autoComboBox = b;
-		    	init();
-		    	return;
-		    }
-		}
-
-		private void init() {
-			setDocument(new AutoDocument());
-			if (isStrict && dataList.size() > 0)
-				setText(dataList.get(0).toString());
-		}
-
-		private String getMatch(String s) {
-			for (int i = 0; i < dataList.size(); i++) {
-				String s1 = dataList.get(i).toString();
-				if (s1 != null) {
-					if (!isCaseSensitive && s1.toLowerCase().startsWith(s.toLowerCase()))
-						return s1;
-					if (isCaseSensitive && s1.startsWith(s))
-						return s1;
-				}
-			}
-
-			return null;
-		}
-		
-		public boolean isCaseSensitive() {
-			return isCaseSensitive;
-		}
-
-		public void setCaseSensitive(boolean flag) {
-			isCaseSensitive = flag;
-		}
-
-		public boolean isStrict() {
-			return isStrict;
-		}
-
-		public void setStrict(boolean flag) {
-			isStrict = flag;
-		}
-
-		public List getDataList() {
-			return dataList;
-		}
-
-		public void setDataList(List list) {
-			if (list == null) {
-				throw new IllegalArgumentException("values can not be null");
-			} else {
-				dataList = list;
-				return;
-			}
-		}
-
-		private List dataList;
-		private boolean isCaseSensitive;
-		private boolean isStrict;
-		private Java2sAutoComboBox autoComboBox;
-	}
 	
-	//TODO Javadocing needed
-	 /*JComboBox auto-complete*/
-	private class Java2sAutoComboBox extends JComboBox {
-		private class AutoTextFieldEditor extends BasicComboBoxEditor {
-			private Java2sAutoTextField getAutoTextFieldEditor() {
-				return (Java2sAutoTextField) editor;
-			}
-
-			AutoTextFieldEditor(java.util.List list) {
-				editor = new Java2sAutoTextField(list, Java2sAutoComboBox.this);
-			}
-		}
-
-		public Java2sAutoComboBox(java.util.List list) {
-			isFired = false;
-			autoTextFieldEditor = new AutoTextFieldEditor(list);
-			setEditable(true);
-			setModel(new DefaultComboBoxModel(list.toArray()) {
-				protected void fireContentsChanged(Object obj, int i, int j) {
-					if (!isFired)
-						super.fireContentsChanged(obj, i, j);
-				}
-			});
-			setEditor(autoTextFieldEditor);
-		}
-
-		public boolean isCaseSensitive() {
-			return autoTextFieldEditor.getAutoTextFieldEditor().isCaseSensitive();
-		}
-
-		public void setCaseSensitive(boolean flag) {
-			autoTextFieldEditor.getAutoTextFieldEditor().setCaseSensitive(flag);
-		}
-
-		public boolean isStrict() {
-			return autoTextFieldEditor.getAutoTextFieldEditor().isStrict();
-		}
-
-		  /*public void setStrict(boolean flag) {
-		    autoTextFieldEditor.getAutoTextFieldEditor().setStrict(flag);
-		  }*/
-
-		public java.util.List getDataList() {
-			return autoTextFieldEditor.getAutoTextFieldEditor().getDataList();
-		}
-
-		public void setDataList(java.util.List list) {
-			autoTextFieldEditor.getAutoTextFieldEditor().setDataList(list);
-			setModel(new DefaultComboBoxModel(list.toArray()));
-		}
-
-		void setSelectedValue(Object obj) {
-			if (isFired) {
-				return;
-			} else {
-				isFired = true;
-				setSelectedItem(obj);
-				fireItemStateChanged(new ItemEvent(this, 701, selectedItemReminder, 1));
-				isFired = false;
-				return;
-			}
-		}
-
-		protected void fireActionEvent() {
-			if (!isFired)
-				super.fireActionEvent();
-		}
-
-		private AutoTextFieldEditor autoTextFieldEditor;
-		private boolean isFired;
-
-	}
 
 	/**
 	 * Custom JMenuBar class which allows the background colour to be changed.
