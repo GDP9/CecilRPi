fbdf9343caf26155ba097c97ad426a486b4ab794
Shreeprabha Aggarwal <shreeprabha.aries@gmail.com>
2013-11-27 20:59:05 +0000

    tada

--- src/org/raspberrypi/cecil/controller/Controller.java
+++ src/org/raspberrypi/cecil/controller/Controller.java
@@ -1,11 +1,18 @@
 package org.raspberrypi.cecil.controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Synthesizer;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 
 import org.raspberrypi.cecil.model.Model;
 import org.raspberrypi.cecil.model.outputstream.ErrorOutputStream;
@@ -40,7 +47,7 @@ public class Controller implements ControllerInterface {
 
 	GpioController gpio;
 	ArrayList<GpioPinDigitalOutput> opin;
-	
+
 	Synthesizer synthesizer;
 	MidiChannel m;
 
@@ -77,8 +84,8 @@ public class Controller implements ControllerInterface {
 		sendToIO = false;
 		usingRPi = System.getProperty("os.name").toLowerCase().equals("linux") && System.getProperty("os.arch").toLowerCase().equals("arm");
 		view.setIOEnabled(usingRPi);
-		
-		
+
+
 		try {
 			synthesizer = MidiSystem.getSynthesizer();
 			if (synthesizer == null) {
@@ -96,23 +103,74 @@ public class Controller implements ControllerInterface {
 
 		m = synthesizer.getChannels()[0];
 		synthesizer.loadInstrument(synthesizer.getDefaultSoundbank().getInstruments()[0]);
-		
+
+		File soundFile = new File("tada.wav");
+		AudioInputStream audioInputStream = null;
+		try
+		{
+			audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+		AudioFormat	audioFormat = audioInputStream.getFormat();
+		SourceDataLine	line = null;
+		DataLine.Info	info = new DataLine.Info(SourceDataLine.class, audioFormat);
+		try {
+			line = (SourceDataLine) AudioSystem.getLine(info);
+
+			/*
+			  The line is there, but it is not yet ready to
+			  receive audio data. We have to open the line.
+			 */
+			line.open(audioFormat);
+		}
+		catch (LineUnavailableException e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+		line.start();
+		int	nBytesRead = 0;
+		byte[]	abData = new byte[128000];
+		while (nBytesRead != -1)
+		{
+			try
+			{
+				nBytesRead = audioInputStream.read(abData, 0, abData.length);
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			if (nBytesRead >= 0)
+			{
+				int	nBytesWritten = line.write(abData, 0, nBytesRead);
+			}
+		}
+		line.drain();
+		line.close();
+
 		/* Welcome note */
-		m.noteOn(62, 64);
-		Thread.sleep(1000);
-		m.noteOn(64, 64);
-		Thread.sleep(500);
-		m.noteOn(62, 64);
-		Thread.sleep(1000);
-		m.noteOn(64, 64);
-		Thread.sleep(500);
-		m.noteOn(62, 64);
-		Thread.sleep(1000);
-		m.noteOn(64, 64);
-//		Thread.sleep(500);
-//		m.noteOn(76, 64);
-//		Thread.sleep(500);
-//		m.noteOn(78, 64);
+		//		m.noteOn(62, 64);
+		//		Thread.sleep(1000);
+		//		m.noteOn(64, 64);
+		//		Thread.sleep(500);
+		//		m.noteOn(62, 64);
+		//		Thread.sleep(1000);
+		//		m.noteOn(64, 64);
+		//		Thread.sleep(500);
+		//		m.noteOn(62, 64);
+		//		Thread.sleep(1000);
+		//		m.noteOn(64, 64);
+
 	}
 
 	/**
@@ -477,7 +535,7 @@ public class Controller implements ControllerInterface {
 		view.setCarryFlag(model.isCarryFlag());
 		view.setZeroFlag(model.isZeroFlag());
 		view.setNegativeFlag(model.isNegativeFlag());
-		
+
 		view.setMemoryAllocation(model.getMemory());
 
 		if (!model.isCompileSuccess() && model.getErrorStream() != null && model.getErrorStream().getErrors() != null) {
@@ -521,7 +579,7 @@ public class Controller implements ControllerInterface {
 			if (gpio == null) {
 				gpio  = GpioFactory.getInstance();
 				piInitialseGPIO();
-				
+
 			}
 		}
 	}
